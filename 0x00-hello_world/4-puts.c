579b9a570fa706b41836be6db2ec8189e68d76fc
olamitosure <olorunsomoolami01@gmail.com>
2022-12-08 13:25:11 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ * main - Main function of the project
+ *
+ *
+ * Return: return 0 all the time
+ * /
+  int main(void)
+  {
+  char show[52];
+
+  strcpy(show, "\"programming is like building a multilingual puzzle");
+  puts(show);
+  return (0);
+  }
