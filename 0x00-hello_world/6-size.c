5d21d9a7f444550343a6bd4d0e245940ee477442
olamitosure <olorunsomoolami01@gmail.com>
2022-12-08 20:01:30 -0800

    4-puts.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+main(void)
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+	printf("Size ofan int:%i) byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(longint));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(longlongint));
+	printf("Size of a float: %i byte(s)\n", sizeof(float));
+	printf("Size of a long long int: %i byte(s)\n", sizeof(longlongint));
+	printf("Size of a float: %i byte(s)\n", sizeof(f
+loat));
+	return(0);
+}
