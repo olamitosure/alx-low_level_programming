582bd160330d669f4d708fd360c152fbe4ec4a3d
olamitosure <olorunsomoolami01@gmail.com>
2022-12-09 22:39:15 -0800

    3-Alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+
+	char c;
+
+	char e;
+
+	c = 'a';
+	e = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(e <= 'Z') {
+			putchar(e);
+				e++;
+		}
+	putchar('\n');
+	return (0);
+}
