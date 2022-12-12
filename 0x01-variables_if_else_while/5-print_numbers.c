1bdf544425e1bb34143348abb0570077ecd991b4
olamitosure <olorunsomoolami01@gmail.com>
2022-12-11 19:09:52 -0800

    5-print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
+ * main - driver code
+ * Description: generate numbers from 0 to 9
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	int a;
+	int n;
 
-	for (a = 0; a <= 9; a++)
-		putchar(a+0);
-	putchar('\n'+0);
+	for (n = 0; n < 10; n++)
+		{
+		printf("%d", n);
+		}
+	printf("\n");
 	return (0);
 }
