7df2dfbae0e9d4d84e13c69d1909219adf24c920
olamitosure <olorunsomoolami01@gmail.com>
2022-12-12 10:09:55 -0800

    102-print comb 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,7 +14,7 @@ int main(void)
 		{
 			putchar(num1);
 			putchar(num2);
-			if (num1 != '8' || (num1 <= '9' && num2 == '9'))
+			if (num1 > num2 && num2 == '9')
 			{
 				putchar(',');
 				putchar(' ');
