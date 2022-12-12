01e171958ec5361986a3d277b187c405f6ed44fc
olamitosure <olorunsomoolami01@gmail.com>
2022-12-12 09:42:43 -0800

    102-print comb 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: This program prints possible combinations of two-digit numbers
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= '9'; num1++)
+	{
+		for (num2 = '0'; num2 <= '9'; num2++)
+		{
+			putchar(num1);
+			putchar(num2);
+			if (num1 != '8' || (num1 <= '9' && num2 == '9'))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
