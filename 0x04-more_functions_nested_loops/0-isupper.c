8672e856daf91d8848ceb6c389c7bcf13ed6364f
olamitosure <olorunsomoolami01@gmail.com>
2022-12-16 01:14:04 -0800

    main.h file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _isupper - checks for upper case alphabet/s
+ * @c: an integer parameter
+ * Return: 1 if c is uppercase, otherwise 0
+ */
+
+int _isupper(int c)
+{
+	char 'uppercase';
+
+	for (uppercase <= 'Z'; uppercase++)
+	{
+		if (c == uppercase)
+		return (1);
+	}
+
+}
