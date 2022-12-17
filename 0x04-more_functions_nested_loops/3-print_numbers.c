99290f32ed2357b3cec39504291ff3842e300649
olamitosure <olorunsomoolami01@gmail.com>
2022-12-16 23:35:34 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - prints nummber from 1 to 9
+ * @c: integers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c >= 'o'; c <= '9'; c++)
+		putchar(c);
+
+	puchar('\n');
+	return (0);
+}
