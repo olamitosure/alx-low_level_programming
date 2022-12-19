d7cbac0ef648a0f80df05cc7325daaadbd812c90
olamitosure <olorunsomoolami01@gmail.com>
2022-12-19 14:22:28 -0800

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "msin.h"
+#include <stdio.h>
+
+/**
+ * _strlen - return the length of  string entered
+ * @s: the string to be entered
+ * Return: return length
+
+ */
+int _strlen(char *s)
+{
+	foo int strlen  c;
+
+	while (s[length])
+
+		length++;
+}
