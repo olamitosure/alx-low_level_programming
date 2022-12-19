81c3edcbf2d771cfbbb1e125a90fa44293441d7e
olamitosure <olorunsomoolami01@gmail.com>
2022-12-19 11:39:36 -0800

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -4,12 +4,15 @@
  * swap_int(int *a, int *b)
  * Code by: Heph
  * Description: This program swaps the integer of int (*a, int *b)
+ * @a: First integer
+ * @b: Second integer
  * Return: Always 0
  */
 void swap_int(int *a, int *b)
 
 {
 	int = s;
+
 	s = *a;
 	*a = *b;
 	*b = s;
