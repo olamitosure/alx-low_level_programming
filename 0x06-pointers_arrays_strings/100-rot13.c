39b2db6daf825c2b2b2aadb8abdb37d964503c00
olamitosure <olorunsomoolami01@gmail.com>
2022-12-21 13:38:12 -0800

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,9 +10,7 @@ char *rot13(char *str)
 
 {
 
-	int indx1 = 0;
-
-	indx2;
+int indx1 = 0, indx2;
 
 char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
 'G', 'H', 'I', 'J', 'K', 'L',
@@ -33,19 +31,18 @@ char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
 'x', 'y', 'z', 'a', 'b', 'c',
 'd', 'e', 'f', 'g', 'h', 'i',
 'j', 'k', 'l', 'm'};
-
-	while (str[indx1])
-	{
-		for (indx2 = 0; indx2 < 52; indx2++)
-		{
-			if (str[indx1] == alphabet[indx2])
-			{
-				str[indx1] = rot13key[indx2];
-			break;
-			}
-		}
-		indx1++;
-	}
-	return (str);
+while (str[indx1])
+{
+for (indx2 = 0; indx2 < 52; indx2++)
+{
+if (str[indx1] == alphabet[indx2])
+{
+str[indx1] = rot13key[indx2];
+break;
+}
+}
+indx1++;
+}
+return (str);
 
 }
