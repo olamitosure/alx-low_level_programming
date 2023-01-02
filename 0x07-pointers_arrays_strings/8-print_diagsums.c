69fed1b9d84c61af5adaca9795cd95aa895ca02f
olamitosure <olorunsomoolami01@gmail.com>
2023-01-02 09:30:32 -0800

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,16 +8,16 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, k, l = 0, r = 0;
+	int i, j, p, l = 0, r = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		k = (i * size) + i;
-		l += *(a + k);
+		p = (i * size) + i;
+		l += *(a + p);
 	}
 	for (j = 0; j < size; j++)
 	{
-		k = (j * size) + (size - 1 - j);
+		p = (j * size) + (size - 1 - j);
 		r += *(a + l);
 	}
 	printf("%i, %i\n", l, r);
