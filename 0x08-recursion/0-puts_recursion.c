bfefe3c7690e906d8ba2d0e2a8bc1ae729858931
olamitosure <olorunsomoolami01@gmail.com>
2023-01-04 06:30:36 -0800

    main.h

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,19 @@
 #include "main.h"
-
 /**
- * _puts_recursion - function prints a string followed by a new line
- * @s: points to string
- * return: void
+ * _puts_recursion - print a string
+ * @s:string
+ * Return:void
  */
 void _puts_recursion(char *s)
 {
-	if (*s == "\0")/*base case*/
+if (*s)
 {
-	_putchar("\n");
-	return ();
-}
 	_putchar(*s);
 	_puts_recursion(s + 1);
+}
+else
+{
+	_putchar('\n');
+}
+
 }
