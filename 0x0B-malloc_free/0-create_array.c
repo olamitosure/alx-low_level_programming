11385311ddeb29a50608bd5f144c339792b86e19
olamitosure <olorunsomoolami01@gmail.com>
2023-01-10 10:58:47 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,25 +2,42 @@
 #include <stdlib.h>
 
 /**
- * create_array - a function that creates an array of chars,
- * and initializes it with a specific char.
- * @size: size of array
- * @c: Array to initialize with
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ * @width: width of the array.
+ * @height: height of the array.
  *
- * Return: pointer to array or NULL
+ * Return: pointer of an array of integers
  */
-
-char *create_array(unsigned int size, char c)
+int **alloc_grid(int width, int height)
 {
-	unsigned int i;
-	char *array = malloc(size);
+int **gridout;
+int i, j;
 
-	if (size == 0 || array == NULL)
-		return (NULL);
+if (width < 1 || height < 1)
+return (NULL);
 
-	for (i = 0; i < size; i++)
-	{
-		array[i] = c;
-	}
-	return (array);
+gridout = malloc(height * sizeof(int *));
+if (gridout == NULL)
+{
+free(gridout);
+return (NULL);
+}
+
+for (i = 0; i < height; i++)
+{
+gridout[i] = malloc(width * sizeof(int));
+if (gridout[i] == NULL)
+{
+for (i--; i >= 0; i--)
+free(gridout[i]);
+free(gridout);
+return (NULL);
+}
+}
+
+for (i = 0; i < height; i++)
+for (j = 0; j < width; j++)
+gridout[i][j] = 0;
+
+return (gridout);
 }
