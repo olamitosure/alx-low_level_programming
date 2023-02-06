40b3f6c9d0674863cf8c6aecf0ba6b286fb54a2f
olamitosure <olorunsomoolami01@gmail.com>
2023-02-06 09:03:13 -0800

    0x15-file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,39 +1,32 @@
 #include "main.h"
-
 /**
  * append_text_to_file - appends text at the end of a file
- * @filename: filename.
- * @text_content: added content.
+ * @filename: file to append the text to
+ * @text_content: content to append into the file
  *
- * Return: 1 if the file exists. -1 if the fails does not exist
- * or if it fails.
+ * Return: 1 on success and -1 on failure
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int rd;
-	int nletters;
-	int rwr;
+int rd, a, b = 0;
 
-	if (!filename)
-		return (-1);
+if (!filename)
+return (-1);
 
-	rd = open(filename, O_WRONLY | O_APPEND);
+fd = open(filename, O_WRONLY | O_APPEND);
+if (rd < 0)
+return (-1);
 
-	if (rd == -1)
-		return (-1);
+if (text_content)
+{
+while (text_content[b])
+b++;
+a = write(rd, text_content, b);
+if (a != b)
+return (-1);
+}
 
-	if (text_content)
-	{
-		for (nletters = 0; text_content[nletters]; nletters++)
-			;
+close(rd);
 
-		rwr = write(fd, text_content, nletters);
-
-		if (rwr == -1)
-			return (-1);
-	}
-
-	close(rd);
-
-	return (1);
+return (1);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,72 +1,51 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- * error_file - checks if files can be opened.
- * @file_from: file_from.
- * @file_to: file_to.
- * @argv: arguments vector.
- * Return: no return.
- */
-void error_file(int file_from, int file_to, char *argv[])
-{
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-}
-
-/**
- * main - check the code for Holberton School students.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
+ * main - copies the content of a file to another file
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
-	char buf[1024];
+int rd_r, fd_w, x, m, n;
+char buf[BUFSIZ];
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		exit(97);
-	}
-
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
-
-	nchars = 1024;
-	while (nchars == 1024)
-	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
-			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
-			error_file(0, -1, argv);
-	}
-
-	err_close = close(file_from);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close rd %d\n", file_from);
-		exit(100);
-	}
-
-	err_close = close(file_to);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close rd %d\n", file_from);
-		exit(100);
-	}
-	return (0);
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+rd_r = open(argv[1], O_RDONLY);
+if (rd_r < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+rd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+while ((x = read(rd_r, buf, BUFSIZ)) > 0)
+{
+if (rd_w < 0 || write(rd_w, buf, x) != x)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+close(rd_r);
+exit(99);
+}
+}
+if (x < 0)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+m = close(rd_r);
+n = close(rd_w);
+if (m < 0 || n < 0)
+{
+if (m < 0)
+dprintf(STDERR_FILENO, "Error: Can't close rd %d\n", fd_r);
+if (n < 0)
+dprintf(STDERR_FILENO, "Error: Can't close rd %d\n", rd_w);
+exit(100);
+}
+return (0);
 }
